03d998e13790f545a4dc374a3b10a9524f423f2b
Alex Raul <alexbraul.ar@gmail.com>
2023-07-30 20:56:36 -0300

    fix import contacts

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -9,7 +9,8 @@ exports.DefaultOptions = {
     },
     webVersion: '2.2322.15',
     webVersionCache: {
-        type: 'local',
+        type: 'remote',
+        remotePath: ' https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2326.8.html ',
     },
     authTimeoutMs: 0,
     qrMaxRetries: 0,
